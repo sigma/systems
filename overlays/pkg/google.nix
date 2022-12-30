c8499d88febbafe5d2834c7ca79b920d6eaa4414
Yann Hodique <yhodique@google.com>
2022-12-30 15:37:29 +0100

    set git exec path properly
    Without this, git only finds helpers in the built-in exec path, not the
    google ones.

--- overlays/pkg/google.nix
+++ overlays/pkg/google.nix
@@ -1,10 +1,20 @@
 final: prev:
 
 let
-  gitPath = if final.stdenv.isDarwin then "/usr/local/git/current/bin" else "/usr/bin";
-  gitGooglePath = if final.stdenv.isDarwin then "/usr/local/git/git-google/bin" else "/usr/bin";
-  figPath = if final.stdenv.isDarwin then "/usr/local/bin" else "/usr/bin";
-  gcertPath = "/usr/local/bin";
+  paths = {
+    gcert = "/usr/local/bin";
+  } // (if final.stdenv.isDarwin then {
+    git = "/usr/local/git/current/bin";
+    gitExec ="/usr/local/git/current/libexec/git-core";
+    gitGoogle = "/usr/local/git/git-google/bin";
+    fig = "/usr/local/bin";
+  } else {
+    git = "/usr/bin";
+    gitExec = "/usr/lib/git-core";
+    gitGoogle = "/usr/bin";
+    fig = "/usr/bin";
+  });
+
 in
 {
   nativeWrapper = final.stdenv.mkDerivation rec {
@@ -37,13 +47,13 @@ in
     dontUnpack = true;
     installPhase = ''
       mkdir -p $out/bin
-      for helper in ${gitPath}/{git,gob}*; do
+      for helper in ${paths.git}/{git,gob}*; do
         bin=`basename $helper`
-        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${gitPath}/$bin
+        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${paths.git}/$bin --set GIT_EXEC_PATH ${paths.gitExec}:${paths.gitGoogle}
       done
-      for helper in ${gitGooglePath}/{git,gob}*; do
+      for helper in ${paths.gitGoogle}/{git,gob}*; do
         bin=`basename $helper`
-        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${gitGooglePath}/$bin
+        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${paths.gitGoogle}/$bin
       done
     '';
   };
@@ -57,7 +67,7 @@ in
     installPhase = ''
       mkdir -p $out/bin
       for bin in chg hg hgd; do
-        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${figPath}/$bin
+        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${paths.fig}/$bin
       done
     '';
   };
@@ -71,7 +81,7 @@ in
     installPhase = ''
       mkdir -p $out/bin
       for bin in gcert gcertstatus gcertdestroy; do
-        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${gcertPath}/$bin
+        makeWrapper ${final.nativeWrapper}/bin/native-wrapper $out/bin/$bin --set NATIVE_WRAPPER_BIN ${paths.gcert}/$bin
       done
 
       cat > $out/bin/gcert_renew.command <<EOF
