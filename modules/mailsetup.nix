a47be8a5286e134748f89c77be0beea90b28a4d1
Yann Hodique <yhodique@google.com>
2023-02-09 16:42:34 +0100

    provide gmi helpers as proper package
    also add launchd support for background mail retrieval

--- modules/mailsetup.nix
+++ modules/mailsetup.nix
@@ -4,55 +4,77 @@ with lib;
 
 let
   cfg = config.programs.mailsetup;
+
+  pkg = pkgs.stdenv.mkDerivation rec {
+    pname = "gmi";
+    version = "v0.1";
+
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/bin
+
+      cat >> $out/bin/gmi-sendmail << 'EOF'
+      #!${pkgs.zsh}/bin/zsh
+
+      MAIL=$(${pkgs.coreutils}/bin/mktemp)
+      trap '{ rm -f -- "$MAIL"; }' EXIT
+      tee "$MAIL" >/dev/null
+
+      FROM=$(${pkgs.procmail}/bin/formail -x From: -c < "$MAIL")
+      PROFILE=""
+
+      ${(builtins.concatStringsSep "\n" (map (prof: ''
+      if [[ "$FROM" =~ "(${builtins.concatStringsSep "|" prof.emails})" ]]; then
+        PROFILE=${prof.name}
+      fi
+      '') user.profiles))}
+
+      if [ -z "$PROFILE" ]; then
+        echo "invalid From address: $FROM" >/dev/stderr
+        exit 1
+      fi
+
+      exec ${pkgs.lieer}/bin/gmi send --quiet -t -C "${config.accounts.email.maildirBasePath}/$PROFILE" < "$MAIL"
+      EOF
+
+      cat >> $out/bin/gmi-sync << 'EOF'
+      #!${pkgs.zsh}/bin/zsh
+
+      SCRIPT_DIR=''${0:a:h}
+
+      ${(builtins.concatStringsSep "\n" (map (prof: ''
+      PROFILE_DIR=${config.accounts.email.maildirBasePath}/${prof.name}
+      if [ -d "$PROFILE_DIR" ]; then
+        cd "$PROFILE_DIR" && ${pkgs.lieer}/bin/gmi pull && "$SCRIPT_DIR/gmi-tag" && ${pkgs.lieer}/bin/gmi push
+        fi
+      '') user.profiles))}
+      EOF
+
+      cat >> $out/bin/gmi-tag << 'EOF'
+      #!${pkgs.zsh}/bin/zsh
+
+      # our config is for all mails
+      ${pkgs.afew}/bin/afew --all --tag --verbose "$@"
+      EOF
+
+      chmod a+x $out/bin/gmi-*
+    '';
+  };
+
 in
 {
   options.programs.mailsetup = {
     enable = mkEnableOption "mailsetup";
+
+    startInterval = mkOption {
+      type = types.nullOr types.int;
+      default = 60;
+      description = "Optional key to start gmi services each N seconds";
+    };
   };
 
   config = mkIf cfg.enable {
-    home.file."bin/gmi-sendmail" = {
-      executable = true;
-      text = ''
-        #!${pkgs.zsh}/bin/zsh
-
-        MAIL=$(${pkgs.coreutils}/bin/mktemp)
-        trap '{ rm -f -- "$MAIL"; }' EXIT
-        tee "$MAIL" >/dev/null
-
-        FROM=$(${pkgs.procmail}/bin/formail -x From: -c < "$MAIL")
-        PROFILE=""
-
-        ${(builtins.concatStringsSep "\n" (map (prof: ''
-        if [[ "$FROM" =~ "(${builtins.concatStringsSep "|" prof.emails})" ]]; then
-          PROFILE=${prof.name}
-        fi
-        '') user.profiles))}
-
-        if [ -z "$PROFILE" ]; then
-          echo "invalid From address: $FROM" >/dev/stderr
-          exit 1
-        fi
-
-        exec ${pkgs.lieer}/bin/gmi send --quiet -t -C "${config.accounts.email.maildirBasePath}/$PROFILE" < "$MAIL"
-        '';
-    };
-
-    home.file."bin/gmi-sync" = {
-      executable = true;
-      text = ''
-        #!${pkgs.zsh}/bin/zsh
-
-        SCRIPT_DIR=''${0:a:h}
-
-        ${(builtins.concatStringsSep "\n" (map (prof: ''
-        PROFILE_DIR=${config.accounts.email.maildirBasePath}/${prof.name}
-        if [ -d "$PROFILE_DIR" ]; then
-          cd "$PROFILE_DIR" && ${pkgs.lieer}/bin/gmi pull && "$SCRIPT_DIR/gmi-tag" && ${pkgs.lieer}/bin/gmi push
-        fi
-        '') user.profiles))}
-      '';
-    };
+    home.packages = [pkg];
 
     home.file.".config/afew/expire.py".text = ''
     from afew.filters.BaseFilter  import Filter
@@ -94,14 +116,16 @@ in
         self.query = "(tag:new AND (%s))" % (self.query)
     '';
 
-    home.file."bin/gmi-tag" = {
-      executable = true;
-      text = ''
-      #!${pkgs.zsh}/bin/zsh
-
-      # our config is for all mails
-      ${pkgs.afew}/bin/afew --all --tag --verbose "$@"
-      '';
+    launchd.agents.gmi-sync = {
+      enable = true;
+      config = {
+        ProgramArguments = [ "${pkg}/bin/gmi-sync" ];
+        KeepAlive = false;
+        RunAtLoad = true;
+        StartInterval = 60;
+        StandardOutPath = "/var/log/gmi/gmi.log";
+        StandardErrorPath = "/var/log/gmi/gmi.log";
+      };
     };
   };
 }
