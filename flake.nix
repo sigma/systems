6c5b771f92c9f435832e6c4f0894a12bb96e294f
Yann Hodique <yhodique@google.com>
2023-02-22 12:20:02 +0100

    move off deprecated devShell attribute
    use devShells.$system.default instead.

--- flake.nix
+++ flake.nix
@@ -157,6 +157,8 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShell = import ./shell.nix { inherit pkgs; };
+        devShells = {
+          default = import ./shell.nix { inherit pkgs; };
+        };
       });
 }
