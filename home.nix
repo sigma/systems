32f2ea319c26b0e8ee11e82a5ca5c65bc27656fb
Yann Hodique <yhodique@google.com>
2023-08-17 14:47:06 +0200

    cleanup outdated comment

--- home.nix
+++ home.nix
@@ -31,7 +31,6 @@ args@{
   programs =
     (import ./settings args) // {
       cloudshell.enable = true;
-      # notmuch python module is broken on darwin
       lieer.enable = true;
       mailsetup.enable = machine.isWork;
       gitui.enable = true;
