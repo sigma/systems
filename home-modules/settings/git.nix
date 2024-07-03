4da7e6e100f0e039038f1d9f7649eb224bd6ef4b
Yann Hodique <yhodique@google.com>
2024-07-03 11:07:30 +0200

    improve delta config

--- home-modules/settings/git.nix
+++ home-modules/settings/git.nix
@@ -38,6 +38,21 @@ in {
   delta.options = {
     syntax-theme = "OneHalfDark";
     navigate = true;
+    features = "decorations";
+
+    interactive = {
+      "keep-plus-minus-markers" = false;
+    };
+
+    decorations = {
+      "commit-decoration-style" = "blue ol";
+      "commit-style" = "raw";
+      "file-style" = "omit";
+      "hunk-header-decoration-style" = "blue box";
+      "hunk-header-file-style" = "red";
+      "hunk-header-line-number-style" = "#067a00";
+      "hunk-header-style" = "file line-number syntax";
+    };
   };
 
   lfs.enable = true;
