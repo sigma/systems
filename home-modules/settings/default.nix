f1d32083ea5d3eeff2e834ec85614255cd18a156
Yann Hodique <yhodique@google.com>
2024-07-19 21:03:33 +0200

    move programs settings to subdir

--- home-modules/settings/default.nix
+++ home-modules/settings/default.nix
@@ -5,20 +5,22 @@
   machine,
   user,
   ...
-}: {
-  programs =
+}: let
+  loader = dir:
     builtins.listToAttrs
     (map
       # foo.nix -> programs.foo = ...
       (p: {
         name = lib.removeSuffix ".nix" p;
-        value = import ./${p} {
+        value = import ./${dir}/${p} {
           inherit config pkgs lib machine user;
         };
       })
       (builtins.filter
         # all .nix files, present company excluded
-        (f: (lib.hasSuffix ".nix" f) && (f != "default.nix"))
-        # in current directory
-        (builtins.attrNames (builtins.readDir ./.))));
+        (f: (lib.hasSuffix ".nix" f))
+        # in directory dir
+        (builtins.attrNames (builtins.readDir ./${dir}))));
+in {
+  programs = loader "programs";
 }
