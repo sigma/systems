228dc90aaf3fd2944fb09cf4fe017c39b4ea89f3
Yann Hodique <yhodique@google.com>
2024-07-31 19:54:30 +0200

    nix-darwin: migrate from deprecated .copy

--- darwin-modules/gmac.nix
+++ darwin-modules/gmac.nix
@@ -8,8 +8,13 @@ lib.optionalAttrs (machine.isWork) {
   # those files are handled by corp and will be reverted anyway, so
   # skip the warning about them being overwritten.
   environment.etc = {
-    "shells".copy = true;
-    "zshrc".copy = true;
+    "shells".knownSha256Hashes = [
+      # default MacOS content. This is safe to override
+      "9d5aa72f807091b481820d12e693093293ba33c73854909ad7b0fb192c2db193"
+    ];
+    "zshrc".knownSha256Hashes = [
+      "7055352423251faa46af6bb3b1754b0119558f5460c4b49d27189a9cac794bc3"
+    ];
     # leave bashrc alone, I don't use bash
     "bashrc".enable = false;
   };
