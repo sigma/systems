f36a2d6347ce3f38fb2d181519bda303d221b23b
Yann Hodique <yhodique@google.com>
2024-09-25 21:06:37 +0200

    fix handling of google-specific code in zsh config

--- overlays/pkg/local/zsh-config.nix
+++ overlays/pkg/local/zsh-config.nix
@@ -17,6 +17,7 @@ stdenv.mkDerivation {
     ];
     exclude = lib.optionals (!google) [
       ./zsh-config/google.plugin.zsh
+      ./zsh-config/p10k.google.zsh
     ];
   };
   dontUnpack = true;
@@ -26,9 +27,89 @@ stdenv.mkDerivation {
       if stdenv.system == "x86_64-linux"
       then "/var/run/ccache/sso-$USER/cookie"
       else "$HOME/.sso/cookie";
+    hiElement =
+      if google
+      then "hi"
+      else "";
+    citcElement =
+      if google
+      then "citc"
+      else "";
+    dirElement =
+      if google
+      then "gdir"
+      else "dir";
+    gcertElement =
+      if google
+      then "gcert"
+      else "";
     generated = writeText "p10k.generated.config.zsh" ''
-      # location of the gcert cookie
-      typeset -g POWERLEVEL9K_CERT_COOKIE_FILE="${gcertCookie}"
+      ${
+        if google
+        then ''
+          typeset -g POWERLEVEL9K_CERT_COOKIE_FILE="${gcertCookie}"
+        ''
+        else ""
+      }
+
+      typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+        ${hiElement}
+        os_icon
+        context
+        ${citcElement}
+        ${dirElement}
+        vcs
+
+        newline
+
+        prompt_char
+      )
+
+      typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+        status
+        command_execution_time
+        background_jobs
+        direnv
+        asdf
+
+        newline
+
+        ${gcertElement}
+        virtualenv
+        anaconda
+        pyenv
+        goenv
+        nodenv
+        nvm
+        nodeenv
+        rbenv
+        rvm
+        fvm
+        luaenv
+        jenv
+        plenv
+        phpenv
+        scalaenv
+        haskell_stack
+        kubecontext
+        terraform
+        aws
+        aws_eb_env
+        azure
+        gcloud
+        google_app_cred
+        nordvpn
+        ranger
+        nnn
+        vim_shell
+        midnight_commander
+        nix_shell
+        todo
+        timewarrior
+        taskwarrior
+      )
+
+      typeset -g POWERLEVEL9K_VCS_BACKENDS=(git hg ${citcElement})
     '';
   in
     ''
