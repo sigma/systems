ee1e9cab750f25fa01f00973ef158b213279a40d
Yann Hodique <yhodique@google.com>
2024-09-27 19:17:24 +0200

    wezterm: load tabline plugin from nix

---
+++ overlays/pkg/local/wezterm-config/config/tabline.lua
@@ -0,0 +1,74 @@
+local wezterm = require('wezterm')
+
+local accent_colors = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+
+local tabline_opts = {
+  options = {
+    icons_enabled = true,
+    theme = "Overnight Slumber",
+    color_overrides = {
+      normal_mode = {
+        a = {},
+        b = { fg = "#A3C3AD" },
+        c = {},
+      },
+      tab = {
+        active = { bg = "#30463C" },
+        inactive = {},
+      }
+    },
+    section_separators = {
+      left = wezterm.nerdfonts.ple_right_half_circle_thick,
+      right = wezterm.nerdfonts.ple_left_half_circle_thick,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.ple_right_half_circle_thin,
+      right = wezterm.nerdfonts.ple_left_half_circle_thin,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.ple_right_half_circle_thick,
+      right = wezterm.nerdfonts.ple_left_half_circle_thick,
+    },
+  },
+  sections = {
+    tabline_a = { },
+    tabline_b = { },
+    tabline_c = { },
+    tab_active = {
+      { Attribute = { Intensity = "Bold" } },
+      { Foreground = { Color = accent_colors.ansi[3] } },
+      "index",
+      "ResetAttributes",
+      { Foreground = { Color = accent_colors.foreground } },
+      { "process", icons_only = true },
+      { Attribute = { Intensity = "Bold" } },
+      { "cwd", max_length = 20, padding = { left = 0, right = 1 } },
+    },
+    tab_inactive = {
+      { Foreground = { Color = accent_colors.ansi[6] } },
+      "index",
+      "ResetAttributes",
+      { "process", icons_only = true },
+      { Attribute = { Intensity = "Bold" } },
+      { "cwd", max_length = 20, padding = { left = 0, right = 1 } },
+    },
+    tabline_x = { },
+    tabline_y = {
+     { "datetime", style = "%a %d %b %Y %H:%M", hour_to_icon = "" },
+    },
+    tabline_z = { "battery" },
+  },
+  extensions = {},
+}
+
+require('tabline.config').set(tabline_opts)
+
+wezterm.on('update-status', function(window)
+  require('tabline.component').set_status(window)
+end)
+
+wezterm.on('format-tab-title', function(tab, _, _, _, hover, _)
+  return require('tabline.tabs').set_title(tab, hover)
+end)
+
+return {}
--- overlays/pkg/local/wezterm-config/wezterm.lua
+++ overlays/pkg/local/wezterm-config/wezterm.lua
@@ -1,74 +1,8 @@
 local wezterm = require('wezterm')
 local Config = require('config')
 
--- require('events.right-status').setup()
--- require('events.left-status').setup()
--- require('events.tab-title').setup()
--- require('events.new-tab-button').setup()
--- require('events.window-title').setup()
-local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
-local accent_colors = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
-
-tabline.setup {
-   options = {
-     icons_enabled = true,
-     theme = "Overnight Slumber",
-     color_overrides = {
-       normal_mode = {
-         a = {},
-         b = { fg = "#A3C3AD" },
-         c = {},
-       },
-       tab = {
-         active = { bg = "#30463C" },
-         inactive = {},
-       }
-     },
-     section_separators = {
-       left = wezterm.nerdfonts.ple_right_half_circle_thick,
-       right = wezterm.nerdfonts.ple_left_half_circle_thick,
-     },
-     component_separators = {
-       left = wezterm.nerdfonts.ple_right_half_circle_thin,
-       right = wezterm.nerdfonts.ple_left_half_circle_thin,
-     },
-     tab_separators = {
-       left = wezterm.nerdfonts.ple_right_half_circle_thick,
-       right = wezterm.nerdfonts.ple_left_half_circle_thick,
-     },
-   },
-   sections = {
-     tabline_a = { },
-     tabline_b = { },
-     tabline_c = { },
-     tab_active = {
-       { Attribute = { Intensity = "Bold" } },
-       { Foreground = { Color = accent_colors.ansi[3] } },
-       "tab_index",
-       "ResetAttributes",
-       { Foreground = { Color = accent_colors.foreground } },
-       { "process", icons_only = true },
-       { Attribute = { Intensity = "Bold" } },
-       { "cwd", max_length = 20, padding = { left = 0, right = 1 } },
-     },
-     tab_inactive = {
-       { Foreground = { Color = accent_colors.ansi[6] } },
-       "tab_index",
-       "ResetAttributes",
-       { "process", icons_only = true },
-       { Attribute = { Intensity = "Bold" } },
-       { "cwd", max_length = 20, padding = { left = 0, right = 1 } },
-     },
-     tabline_x = { },
-     tabline_y = {
-      { "datetime", style = "%a %d %b %Y %H:%M", hour_to_icon = "" },
-     },
-     tabline_z = { "battery" },
-   },
-   extensions = {},
-}
-
 return Config:init()
+   :append(require('config.tabline'))
    :append(require('config.appearance'))
    :append(require('config.bindings'))
    :append(require('config.domains'))
