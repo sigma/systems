5d053e4d929b1309b367b207c5498d5b18c4bcc9
Yann Hodique <yann.hodique@gmail.com>
2024-10-03 08:30:14 +0200

    add 1password module

---
+++ darwin-modules/apps/1password.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.programs.onepassword;
+in {
+  options = {
+    programs.onepassword = {
+      enable = mkEnableOption "1Password";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    homebrew.casks = [
+      {
+        name = "1password";
+        args = {appdir = "/Applications";};
+      }
+    ];
+  };
+}
