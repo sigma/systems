0b73e8b75715b04487675fe4710e4869d36a4920
Yann Hodique <yann.hodique@gmail.com>
2024-10-03 08:28:03 +0200

    change karabiner strategy
    Looks like different MBP models expose internal keyboard differently.
    So let's leave the possibility for the machine to set it accordingly.
    
    Also, just ignore keychron keyboards instead of bending over backwards
    to accommodate the weird internal keyboard management.

--- darwin-modules/apps/karabiner.nix
+++ darwin-modules/apps/karabiner.nix
@@ -6,10 +6,20 @@
 }:
 with lib; let
   cfg = config.programs.karabiner;
-  cfgTxt = builtins.toJSON (import ./karabiner/config.nix {inherit lib;});
+  cfgTxt = builtins.toJSON (import ./karabiner/config.nix {inherit lib cfg;});
 in {
   options.programs.karabiner = {
     enable = mkEnableOption "Karabiner";
+
+    internalKeyboardID = mkOption {
+      type = types.raw;
+      description = "The internal keyboard ID for a laptop";
+      default = {
+        vendor_id = 1452;
+        product_id = 835;
+        is_keyboard = true;
+      };
+    };
   };
 
   config = mkIf cfg.enable {
--- darwin-modules/apps/karabiner/config.nix
+++ darwin-modules/apps/karabiner/config.nix
@@ -1,19 +1,22 @@
-{lib, ...}: let
+{
+  lib,
+  cfg,
+  ...
+}: let
   helpers = import ./helpers.nix {inherit lib;};
-  internalID = {
-    vendor_id = 1452;
-    product_id = 835;
-    is_keyboard = true;
-  };
   kinesisPedalID = {
     vendor_id = 10730;
     product_id = 256;
     is_keyboard = true;
   };
+  keychronID = {
+    vendor_id = 13364;
+    is_keyboard = true;
+  };
   forInternalID = [
     {
       type = "device_if";
-      identifiers = [internalID];
+      identifiers = [cfg.internalKeyboardID];
     }
   ];
   remap = from: to:
@@ -25,11 +28,6 @@
       inherit from to;
       keyType = "consumer_key_code";
     };
-  remap_vendor = from: to:
-    helpers.remap {
-      inherit from to;
-      keyType = "apple_vendor_keyboard_key_code";
-    };
 in {
   profiles = [
     {
@@ -37,27 +35,15 @@ in {
       selected = true;
       devices = [
         {
-          identifiers = internalID;
+          identifiers = cfg.internalKeyboardID;
           simple_modifications = [
             (remap "left_option" "left_command")
             (remap "left_command" "left_option")
             (remap "right_command" "right_option")
             (remap "right_option" "right_command")
           ];
-          # because of the hack below, we need to remap the function keys for the internal keyboard.
           fn_function_keys = [
-            (remap_fn "f1" "display_brightness_decrement")
-            (remap_fn "f2" "display_brightness_increment")
-            (remap_vendor "f3" "mission_control")
-            (remap_vendor "f4" "spotlight")
-            (remap_fn "f5" "dictation")
             (remap "f6" "f16") # "do not disturb" key is not supported natively by Karabiner. Remap to f16, and rely on the keyboard settings being aligned.
-            (remap_fn "f7" "rewind")
-            (remap_fn "f8" "play_or_pause")
-            (remap_fn "f9" "fast_forward")
-            (remap_fn "f10" "mute")
-            (remap_fn "f11" "volume_decrement")
-            (remap_fn "f12" "volume_increment")
           ];
         }
         {
@@ -66,13 +52,12 @@ in {
             (remap_fn "left_option" "dictation")
           ];
         }
+        {
+          # ignore my keychron keyboards, they're configured natively.
+          identifiers = keychronID;
+          ignore = true;
+        }
       ];
-
-      # hack to prevent karabiner from remapping function keys in external keyboards
-      fn_function_keys = let
-        fKeys = map (n: "f${toString n}") (lib.range 1 12);
-      in
-        map (key: remap key key) fKeys;
 
       complex_modifications = {
         rules = [
