7ec8509a998473340f7562f838154e758364535c
Yann Hodique <yann.hodique@gmail.com>
2024-10-05 11:23:15 +0200

    laptop: improve config readability

--- darwin-modules/policy/laptop.nix
+++ darwin-modules/policy/laptop.nix
@@ -1,26 +1,24 @@
 {lib, ...}: {
-  programs.karabiner = with lib; {
-    # I always use karabiner on my laptops
-    enable = mkForce true;
-
-    # My docking station.
-    # ignore the q1 max keyboard and its wireless link
-    ignoreKeyboards = [
-      # q1 max
-      {
-        vendorId = 13364;
-        productId = 2064;
-      }
-      # wireless link
-      {
-        vendorId = 13364;
-        productId = 53296;
-      }
-    ];
-
-    pedal = {
+  programs.karabiner = with lib; let
+    keychron = pid: {
+      vendorId = 13364;
+      productId = pid;
+    };
+    q1Max = keychron 2064;
+    wirelessLink = keychron 53296;
+    kinesisPedal = {
       vendorId = 10730;
       productId = 256;
     };
+  in {
+    # I always use karabiner on my laptops
+    enable = mkForce true;
+
+    # My docking station for laptops.
+    ignoreKeyboards = [
+      q1Max
+      wirelessLink
+    ];
+    pedal = kinesisPedal;
   };
 }
