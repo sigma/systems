f8552dd085e562662a715b12d4b5de2d6e8c9e0b
Yann Hodique <yann.hodique@gmail.com>
2024-10-16 18:17:02 +0200

    sesh: add second brain session

--- home-modules/settings/programs/sesh.nix
+++ home-modules/settings/programs/sesh.nix
@@ -7,5 +7,10 @@
       path = "~/Downloads";
       startupScript = "${pkgs.eza}/bin/eza";
     }
+    {
+      name = "Second Brain 🧠";
+      path = "~/org";
+      startupScript = "${pkgs.emacs}/bin/emacs -f org-roam-dailies-goto-today";
+    }
   ];
 }
