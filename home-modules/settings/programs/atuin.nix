6b65294cd3ed8400efbded11083a535a99fad666
Yann Hodique <yann.hodique@gmail.com>
2024-11-04 10:31:38 +0100

    atuin: configure in separate settings module

---
+++ home-modules/settings/programs/atuin.nix
@@ -0,0 +1,16 @@
+{...}: {
+  enable = true;
+
+  settings = {
+    # keybindings behaviors
+    "enter_accept" = true;
+    "filter_mode_shell_up_key_binding" = "directory";
+
+    # sync
+    "sync.records" = true;
+
+    # style
+    "style" = "compact";
+    "inline_height" = 30;
+  };
+}
