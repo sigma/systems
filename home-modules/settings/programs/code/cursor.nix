beb9a7edc137729e374195aa6e6258dd4ba43598
Yann Hodique <yann.hodique@gmail.com>
2024-11-07 13:19:53 +0100

    vscode: move magit exclusion for cursor to emacs module

--- home-modules/settings/programs/code/cursor.nix
+++ home-modules/settings/programs/code/cursor.nix
@@ -4,10 +4,6 @@
       "plaintext"
       "markdown"
       "scminput"
-
-      # for all intents and purposes, we should treat magit editors as
-      # readonly. Plus we use tab extensively in there.
-      "magit"
     ];
   };
 }
--- home-modules/settings/programs/code/emacs.nix
+++ home-modules/settings/programs/code/emacs.nix
@@ -28,4 +28,12 @@
       "command" = "-emacs-mcx.isearchExit";
     }
   ];
+
+  userSettings = {
+    "cursor.cpp.disabledLanguages" = [
+      # for all intents and purposes, we should treat magit editors as
+      # readonly. Plus we use tab extensively in there.
+      "magit"
+    ];
+  };
 }
