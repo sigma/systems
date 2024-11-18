c9cd8a0aa1eae3c17c4ea42a9bcff1bd142e402d
Yann Hodique <yann.hodique@gmail.com>
2024-11-18 14:20:11 +0100

    nix: stop notifying about dirty repositories
    It happens all the time, and it's annoying.

--- darwin-modules/nix.nix
+++ darwin-modules/nix.nix
@@ -20,6 +20,7 @@
     ''
       auto-optimise-store = true
       allow-import-from-derivation = true
+      warn-dirty = false
     ''
     + lib.optionalString (pkgs.system == "aarch64-darwin") ''
       extra-platforms = x86_64-darwin aarch64-darwin
