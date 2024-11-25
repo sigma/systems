4707fae2ed6be9990414732d8e10f84b1efd608d
Yann Hodique <yann.hodique@gmail.com>
2024-11-25 12:42:14 +0100

    fix nix config

--- nixos-modules/nix.nix
+++ nixos-modules/nix.nix
@@ -1,5 +1,15 @@
 { machine, lib, ... }:
 {
+  nix.settings.substituters = [
+    "https://cache.nixos.org/"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+  nix.settings.trusted-users = [
+    "root"
+  ];
+
   nix.extraOptions = ''
     auto-optimise-store = true
     allow-import-from-derivation = true
