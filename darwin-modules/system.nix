51854e5d391a657aba00e0f1df02687bfdc4b4aa
Yann Hodique <yann.hodique@gmail.com>
2024-12-08 17:05:38 +0100

    fish: add homebrew to path

--- darwin-modules/system.nix
+++ darwin-modules/system.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  user,
+  ...
+}: {
   programs.fish.enable = true;
   programs.fish.useBabelfish = true;
 
@@ -19,6 +24,10 @@
     enable = true;
     onActivation.cleanup = "zap";
   };
+
+  home-manager.users.${user.login}.programs.fish.interactiveShellInit = ''
+    fish_add_path ${config.homebrew.brewPrefix}
+  '';
 
   security.pam.touchid.enable = true;
   security.pam.reattach.package = pkgs.stable.pam-reattach;
