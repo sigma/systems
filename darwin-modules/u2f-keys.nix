6f5274546d95ba6f3e36a8acc76a2147c30f894f
Yann Hodique <yann.hodique@gmail.com>
2025-01-02 16:24:12 +0100

    darwin: add home key

--- darwin-modules/u2f-keys.nix
+++ darwin-modules/u2f-keys.nix
@@ -10,8 +10,11 @@ with lib; {
   security.pam.u2f.authorizations =
     [
       # portable keys
+      "${user.login}:sKyQL1tKpsjH1WsM+fZXysX4Cdff/N6FjS4G3SinILb5TZOF6JrFI4hdIYzmn+2kYXJru+DMHtSj/NRYrhDldA==,kP7KSC4S4Fqgei6HBZ/BFpS/Chun+4Kn6H0fZ2OyFEvq6y9jkqz+IWTrlk3XiU4DoBcdF/uj+Muofq/DTaQCMw==,es256,+presence"
     ]
     ++ optionals (machine.name == "ash.local") [
+      # home dock yubikey
+      "${user.login}:QKdeeNCt3nfvAUwc2bx4A/Lamg+dtCdU5Mdsq+L+GxBkbr0eO6oWsVwE7NmzMMVhbljYDs3CDwh34zWem9pqwQ==,NYMKNF72hkXSTZCWU4sQXdy0oGbmT6B0WSuuEabso5YzRM//ZU5EEOr9TJBP64tc6mliCAIBFBdoQPi6Mcdw0g==,es256,+presence"
       # laptop low-profile key
       "${user.login}:mEJg6bvtXfOO8r3USlUbN6xaW87kBR7xAlVTfeFxdQSAh06vNXqOLgbjQu4XHbM1qdmEQNlfhrErxfR6Jv5M8A==,iiS2fAX/OMD79/nSPRtG/OPVn326dvU/qV2EkxAfVvasuE2I98odrFgGA3IRJyBF8ucC+sEMt/uVekIs01uqhA==,es256,+presence"
     ]
