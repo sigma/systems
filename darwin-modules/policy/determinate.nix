5059eff75a9c9f3441cd781d11950508d583993f
Yann Hodique <yann.hodique@gmail.com>
2025-05-27 17:48:04 +0200

    restore registry for determinate nix

--- darwin-modules/policy/determinate.nix
+++ darwin-modules/policy/determinate.nix
@@ -4,6 +4,7 @@
   ...
 }:
 with lib; {
+  # disable nix management as we're using determinate nix.
   nix.enable = mkForce false;
 
   # determinate nix config. Only nix.custom.conf can be used to override
@@ -14,4 +15,11 @@ with lib; {
 
     ${config.nix.extraOptions}
   '';
+
+  # restore ability to populate registry, which is normally guarded by
+  # nix.enable.
+  environment.etc."nix/registry.json".text = builtins.toJSON {
+    version = 2;
+    flakes = mapAttrsToList (n: v: {inherit (v) from to exact;}) config.nix.registry;
+  };
 }
