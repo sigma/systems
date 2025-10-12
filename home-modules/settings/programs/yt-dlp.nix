7e0b419646965a9b622afd339d03a2f28a531f93
Yann Hodique <yann.hodique@gmail.com>
2025-10-12 22:32:03 +0200

    chore: simplify yt-dlp config

--- home-modules/settings/programs/yt-dlp.nix
+++ home-modules/settings/programs/yt-dlp.nix
@@ -9,31 +9,35 @@
     mtime = false;
   };
 
-  homes = [
-    {
-      root = "Music";
-      settings = {
-        download-archive = "downloaded.txt";
-        extract-audio = true;
-        extractor-args = "soundcloud:formats=http_mp3";
-        output = "%(extractor_key)s/%(artist)s - %(title)s.%(ext)s";
-        preset-alias = [
-          "mp3"
-          "sleep"
-        ];
-        retry-sleep = [
-          "linear=1::2"
-          "fragment:exp=1:20"
-          "extractor:300"
-        ];
-      };
-    }
-    {
-      root = "Video/Udemy";
-      settings = {
-        download-archive = "downloaded.txt";
-        output = "%(playlist)s/%(chapter_number)s - %(chapter)s/%(title)s.%(ext)s";
-      };
-    }
-  ];
+  homes =
+    let
+      download-archive = "downloaded.txt";
+    in
+    [
+      {
+        root = "Music";
+        settings = {
+          inherit download-archive;
+          extract-audio = true;
+          extractor-args = "soundcloud:formats=http_mp3";
+          output = "%(extractor_key)s/%(artist)s - %(title)s.%(ext)s";
+          preset-alias = [
+            "mp3"
+            "sleep"
+          ];
+          retry-sleep = [
+            "linear=1::2"
+            "fragment:exp=1:20"
+            "extractor:300"
+          ];
+        };
+      }
+      {
+        root = "Video/Udemy";
+        settings = {
+          inherit download-archive;
+          output = "%(playlist)s/%(chapter_number)s - %(chapter)s/%(title)s.%(ext)s";
+        };
+      }
+    ];
 }
