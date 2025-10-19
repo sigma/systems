a59625f76f81c1680903e0147db4036a3961d76e
Yann Hodique <yann.hodique@gmail.com>
2025-10-19 14:32:46 +0200

    chore: remove invalid nix options

--- nixos-modules/nix.nix
+++ nixos-modules/nix.nix
@@ -18,8 +18,6 @@
     extra-experimental-features = nix-command flakes
   ''
   + lib.optionalString machine.features.determinate ''
-    lazy-trees = true
-    eval-cores = 0 # Evaluate across all cores
     extra-substituters https://install.determinate.systems
     extra-trusted-public-keys cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM=
   '';
