fb97fb25bc1f29762c21533360e57ee6a3c9a254
Yann Hodique <yann.hodique@gmail.com>
2025-12-07 21:20:28 +0100

    feat: optionally load lazyvim from neovim config

--- nixos-modules/dev.nix
+++ nixos-modules/dev.nix
@@ -10,7 +10,10 @@
     ];
 
     extraLuaConfig = ''
-      -- require("lazyvim_init")
+      local lazyvim_init_path = vim.fn.expand("$HOME") .. "/.config/nvim/lazyvim_init.lua"
+      if vim.fn.filereadable(lazyvim_init_path) == 1 then
+        pcall(vim.cmd, 'source ' .. lazyvim_init_path)
+      end
     '';
   };
 
