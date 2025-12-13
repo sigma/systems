d9a2f71a99e18cdf2b645a6e3181891cdde78f27
Yann Hodique <yann.hodique@gmail.com>
2025-12-13 17:47:32 +0100

    feat(neovim): enable breadcrumbs on inactive windows

--- home-modules/editors/neovim/ui/breadcrumbs.nix
+++ home-modules/editors/neovim/ui/breadcrumbs.nix
@@ -15,8 +15,60 @@ in
       enable = true;
       # Use nvim-navic as the source
       source = "nvim-navic";
-      # Automatically add to lualine winbar
-      lualine.winbar.enable = true;
+      # Disable nvf's default winbar, we'll configure it manually
+      lualine.winbar.enable = false;
     };
+
+    # Configure winbar for both active and inactive windows
+    programs.neovim-ide.luaConfigPost."15-winbar" = ''
+      -- Custom winbar showing breadcrumbs on all windows
+      local navic = require('nvim-navic')
+
+      local function get_winbar()
+        if navic.is_available() then
+          local location = navic.get_location()
+          if location ~= "" then
+            return "%#WinBar# " .. location
+          end
+        end
+        return ""
+      end
+
+      -- Winbar for inactive windows (dimmed)
+      local function get_inactive_winbar()
+        if navic.is_available() then
+          local location = navic.get_location()
+          if location ~= "" then
+            return "%#WinBarNC# " .. location
+          end
+        end
+        return ""
+      end
+
+      -- Set up winbar via lualine
+      require('lualine').setup({
+        options = {
+          disabled_filetypes = {
+            winbar = { 'neo-tree', 'dashboard', 'alpha', 'starter', 'toggleterm', 'TelescopePrompt' },
+          },
+        },
+        winbar = {
+          lualine_c = {
+            {
+              function() return get_winbar() end,
+              color = { bg = 'NONE' },
+            },
+          },
+        },
+        inactive_winbar = {
+          lualine_c = {
+            {
+              function() return get_inactive_winbar() end,
+              color = { bg = 'NONE' },
+            },
+          },
+        },
+      })
+    '';
   };
 }
