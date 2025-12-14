f16935e06a61284356044decd923d5c77d5207d3
Yann Hodique <yann.hodique@gmail.com>
2025-12-14 11:47:20 +0100

    feat(neovim): add buffer auto-reload

--- home-modules/editors/neovim/lua/user/core.lua
+++ home-modules/editors/neovim/lua/user/core.lua
@@ -22,9 +22,41 @@ function M.suppress_warnings()
   end
 end
 
+-- Setup auto-reload for external file changes
+function M.setup_autoread()
+  -- Enable autoread to allow reloading files changed outside Neovim
+  vim.o.autoread = true
+
+  -- Create autocommands to trigger checktime on various events
+  local group = vim.api.nvim_create_augroup('AutoReload', { clear = true })
+
+  -- Check for file changes when gaining focus or entering buffer
+  vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+    group = group,
+    pattern = '*',
+    callback = function()
+      if vim.fn.mode() ~= 'c' then
+        vim.cmd('checktime')
+      end
+    end,
+  })
+
+  -- Notify when file changes are detected
+  vim.api.nvim_create_autocmd('FileChangedShellPost', {
+    group = group,
+    pattern = '*',
+    callback = function()
+      vim.notify('File changed on disk. Buffer reloaded.', vim.log.levels.INFO)
+    end,
+  })
+end
+
 -- Setup borders and diagnostics (run after plugins)
 function M.setup(opts)
   opts = opts or {}
+
+  -- Setup auto-reload for external file changes
+  M.setup_autoread()
 
   -- Global border style for all floating windows
   local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
