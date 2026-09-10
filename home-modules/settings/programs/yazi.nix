aabe6bc121d1bfa0d5756b4a0acd9f8ea2004668
Yann Hodique <yann.hodique@gmail.com>
2026-09-10 08:14:08 +0200

    fix(yazi): pin shellWrapperName to the new "y" default
    home-manager 26.05 moved the default of `programs.yazi.shellWrapperName`
    from `yy` to `y`, and warns until the option is set explicitly. Adopt the
    new default rather than the legacy one.

--- home-modules/settings/programs/yazi.nix
+++ home-modules/settings/programs/yazi.nix
@@ -2,4 +2,7 @@
 {
   inherit (config.features.shell) enable;
   enableFishIntegration = true;
+  # Pin the shell wrapper name: home-manager 26.05 moved the default from "yy"
+  # to "y" and warns until the option is set explicitly. We take the new default.
+  shellWrapperName = "y";
 }
